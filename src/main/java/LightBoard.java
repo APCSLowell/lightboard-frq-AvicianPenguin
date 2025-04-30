5a6788ee583b6dcf6a62e8ad763f02ed46b3ed52
Penguin <aleksandervolkov2007@gmail.com>
2025-04-30 09:16:54 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -13,7 +13,7 @@ public class LightBoard
     lights = new boolean[numRows][numCols];
     for(int i = 0; i<numRows; i++)
     {
-      for(int j = 0; i<numCols; j++)
+      for(int j = 0; j<numCols; j++)
       {
         double rnd = Math.random();
         lights[i][j] = rnd<0.4;
